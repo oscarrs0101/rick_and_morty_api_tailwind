188f8842d88b529bd4f47c941745469fe9e8589c
Oscar Rios <oscarrios0101@gmail.com>
2022-06-27 01:56:59 -0500

    configuracion de tailwind y daisyUI correcta, con funcionalidad basica de la app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react';
+import CharacterList from './components/CharacterList';
+import Character from './components/Character';
+import Carrousel from './components/Carrousel';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">asdasd</h1>
+      <Carrousel />
+      <CharacterList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CharacterList.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import Character from './Character';
+const CharacterList = () => {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('https://rickandmortyapi.com/api/character');
+      const data = await response.json();
+      console.log(data.results);
+      setCharacters(data.results);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {characters.map((character) => {
+        return <Character key={character.id} character={character} />;
+      })}
+    </div>
+  );
+};
+
+export default CharacterList;
